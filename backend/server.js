8e42df2c45e8659830855df956f9d6317f0c84bd
SoulDevt <souleyboyz@gmail.com>
2023-01-07 13:33:13 +0100

    user api - authorization middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db')
 const colors = require('colors')
 
 const userRoutes = require("./routes/userRoutes")
+const chatRoutes = require("./routes/chatRoutes")
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 })
 
 app.use("/api/user", userRoutes)
+app.use("/api/chat", chatRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
